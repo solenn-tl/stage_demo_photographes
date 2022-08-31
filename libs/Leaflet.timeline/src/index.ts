677d06a0606c209d5e5b811e2bf307b99ae8aeb2
solenn-tl <solenn.tual@gmail.com>
2022-08-31 10:26:09 +0200

    Add time slider

--- libs/Leaflet.timeline/src/index.ts
+++
@@ -1,3 +0,0 @@
-import './Timeline.ts';
-import './TimelineSliderControl.ts';
-import './leaflet.timeline.css';
--- map.js
+++ map.js
@@ -21,26 +21,29 @@ var GeoportailFrance_plan = L.tileLayer('https://wxs.ign.fr/{apikey}/geoportail/
     style: 'normal'
 });
 
+/*Map*/
+var map = L.map('map',{
+    fullscreenControl: true,
+    fullscreenControlOptions: {
+        position: 'topleft'
+    }
+}).setView([48.859972,2.347984],13);
+
 /*Functions*/
-function getColor(data) {
-    return data == 'photo' ? '#C70039' :
-            data == 'daguer' ? '#1471BE':
-            data == 'opti' ? '#1471BE':
-            '#D60FCA';
-}
 
 var geojsonMarkerOptionsExtract = {
     radius:4,
-    fillColor: "#0FB7D9",
+    fillColor: "#1a2a78",
     color: "#ffffff",
     weight: 1,
     opacity: 1,
     fillOpacity: 1
 };
 
+
 var geojsonMarkerOptionsRef = {
     radius:4,
-    fillColor: "#66df6a",
+    fillColor: "#DA8513",
     color: "#ffffff",
     weight: 1,
     opacity: 1,
@@ -59,54 +62,113 @@ function pointToLayerRef(feature,latlng) {
 
 function onEachFeature(feature, layer) {
     // Pop-up content for directories data
-    if (feature.properties && feature.properties.person) {
+    if (feature.properties.unique_id) {
         texte = '<h4>'+feature.properties.person+'</h4>'+
         '<p><b>Adresse (annuaire)</b> : ' + feature.properties.number + ' '+ feature.properties.street + '<br>'+ 
         '<b>Activité</b> : ' + feature.properties.activity + '<br>'+ 
         '<b>Annuaire</b> : ' + feature.properties.directory + '</br>'+
         '<b>Année de publication</b> : ' + feature.properties.year + '<br></p>'
         layer.bindPopup(texte);
-    } else if (feature.properties && feature.properties.secteur) {
-        texte = '<h4>'+feature.properties.nom + '</h4>'+
-        '<p><b>Adresse</b> : ' + feature.properties.street + ' ' + feature.properties.number + '<br>'
+    } else if (feature.properties.secteur) {
+        if (feature.properties.prénoms && feature.properties.nom){
+            texte = '<h4>' + feature.properties.prénoms + ' ' + feature.properties.nom + '</h4>'
+        } else if (feature.properties.prénoms == null && feature.properties.nom) {
+            texte = '<h4>' + feature.properties.nom + '</h4>'
+        }
+        texte += '<p><b>Adresse</b> : ' + feature.properties.street + ' ' + feature.properties.number + '<br>'
         if (feature.properties.rue_2) {
             texte += '<b>Seconde adresse </b> : ' + feature.properties.rue_2 + '<br>'
         }
-        texte += "<b>Période d'activité</b> : " + feature.properties.date + '<br></p>'
+        if (feature.properties.date)
+            {texte += "<b>Période d'activité</b> : " + feature.properties.date + '<br></p>'}
+        if (feature.properties.note) {
+            texte += "<p>" + feature.properties.note + "</p>"
+        }
         layer.bindPopup(texte);
     }
 };
 
-function getInterval (feature) {
-      // earthquake data only has a time, so we'll use that as a "start"
-      // and the "end" will be that + some value based on magnitude
-      // 18000000 = 30 minutes, so a quake of magnitude 5 would show on the
-      // map for 150 minutes or 2.5 hours
-      console.log(feature.properties.year +"01-01")
-      return {
-        start: feature.properties.year +"01-01",
-        end: feature.properties.year+"12-31",
-      };
-      
-    };
+/*Timeslider - INIT*/
+var slidervar = document.getElementById('slider');
+noUiSlider.create(slidervar, {
+    connect: true,
+    start: [ 1870, 1875 ],
+    step:1,
+    range: {
+        min: 1790,
+        max: 1910
+    },
+    format: wNumb({
+        decimals: 0
+    }),
+});
+
+//CONNECT SLIDER WITH DATA
+//Set default value on input number
+document.getElementById('input-number-min').setAttribute("value", 1870);
+document.getElementById('input-number-max').setAttribute("value", 1875);
+//Event on input number
+var inputNumberMin = document.getElementById('input-number-min');
+var inputNumberMax = document.getElementById('input-number-max');
+inputNumberMin.addEventListener('change', function(){
+    slidervar.noUiSlider.set([this.value, null]);
+});
+inputNumberMax.addEventListener('change', function(){
+    slidervar.noUiSlider.set([null, this.value]);
+});
 
 /*Data*/
 var url_extract = "./data/par_activite_geocoded_unique.geojson"
 
 //Extracted data
-var extract = L.timeline(null, {
-    pointToLayer:pointToLayerExtract,
-    waitToUpdateMap: true,
-    onEachFeature: onEachFeature,
-    getInterval: getInterval,
-    },
-  );
 
+var extract = L.geoJSON(null,{
+    onEachFeature: onEachFeature,
+    pointToLayer:pointToLayerExtract,
+    filter: function(feature, layer) {
+        return (feature.properties.year <= 1880) && (feature.properties.year >= 1875);
+        }
+});
   // Get GeoJSON data et création
 $.getJSON(url_extract, function(data) {
     extract.addData(data);
 });
+var extractgroup =L.featureGroup();
+extract.addTo(extractgroup);
 
+var extract;
+
+fetch(url_extract)
+.then(function(response){ return response.json() })
+.then(function(data){
+
+  var photo = L.geoJson(data, { 
+    onEachFeature: onEachFeature,
+    pointToLayer:pointToLayerExtract,
+    filter:function(feature, layer) {   
+        return feature.properties.has_photo == 1
+    }
+    });
+
+var daguer = L.geoJson(data, { 
+    onEachFeature: onEachFeature,
+    pointToLayer:pointToLayerExtract,
+    filter:function(feature, layer) {   
+        return feature.properties.has_daguer == 1
+    }
+    });
+
+var opti = L.geoJson(data, { 
+    onEachFeature: onEachFeature,
+    pointToLayer:pointToLayerExtract,
+    filter:function(feature, layer) {   
+        return feature.properties.has_opti == 1
+    }
+    });
+
+    //extract = L.layerGroup([photo, daguer, opti]);
+  
+/*
 //Par mot-clé
 ////Photo
 var photo = L.geoJSON(null,{
@@ -140,7 +202,7 @@ var opti = L.geoJSON(null,{
 });
 $.getJSON(url_extract, function(data) {
         opti.addData(data);
-});
+});*/
 
 //Ref
 var url_ref = "./data/reference_geocoded_unique.geojson"
@@ -154,27 +216,6 @@ var ref = L.geoJSON(null,{
 $.getJSON(url_ref, function(data) {
         ref.addData(data);
 });
-
-/*Timeline Control*/
-var timelineControl;
-
-timelineControl = L.timelineSliderControl({
-    formatOutput: function (date) {
-        return new Date(date).toLocaleDateString();
-    },
-    enableKeyboardControls: true,
-});
-
-/*Map*/
-var map = L.map('map',{
-    fullscreenControl: true,
-    fullscreenControlOptions: {
-        position: 'topleft'
-    }
-}).setView([48.859972,2.347984],13);
-
-timelineControl.addTo(map);
-timelineControl.addTimelines(extract);
 
 /*Layer Control*/
 
@@ -213,28 +254,93 @@ var overLayers = [
         layer: ref
     },
     {
-        active: false,
-        name: "Extraction - Tout",
-        layer: extract
+        active: true,
+        name: "Extraction (complète)",
+        layer: extractgroup,
+        title:'all'
     },
-    {group:"Extraction - Par mot-clé",
+    {group:"Extraction (par mot-clé)",
     layers :[
         {
-            active:true,
+            active:false,
             name: "photo",
             layer: photo,
+            title:'photo'
         },
         {
             active: false,
             name: "daguer",
-            layer: daguer
+            layer: daguer,
+            title:'daguer'
         },
         {
             active: false,
             name: "opti",
-            layer: opti}
+            layer: opti,
+            title:'opti'}
     ]}
 ];
 
-map.addControl( new L.Control.PanelLayers(baseLayers, overLayers,
+map.addControl(new L.Control.PanelLayers(baseLayers, overLayers,
     {title:'<h3 id="panel">Photographes</h3>'}));
+
+/*
+function filterData (feature,layer) {
+    if ($('#photo').is(':checked') && $('#daguer').is(':checked') && $('#opti').is(':checked')) {
+        console.log('all')
+    } else if ($('#photo').is(':checked') && $('#daguer').is(':checked') && $( "#opti" ).prop( "checked", false )) {
+    } else if ($('#photo').is(':checked') && $( "#opti" ).prop( "checked", false ) && $( "#opti" ).prop( "checked", false )) {
+    } else if ($('#daguer').is(':checked') && $('#photo').is(':checked') && $( "#opti" ).prop( "checked", false )) {
+    } else if ($('#daguer').is(':checked') && $( "#photo" ).prop( "checked", false ) && $( "#opti" ).prop( "checked", false )) {
+    } else if ($('#opti').is(':checked') && $('#photo').is(':checked') && $( "#daguer" ).prop( "checked", false )) {
+    } else if ($('#opti').is(':checked') && $( "#photo" ).prop( "checked", false ) && $( "#daguer" ).prop( "checked", false )) {
+    }
+}*/
+
+var photobox = document.getElementById('photo').checked;
+var daguerbox = document.getElementById('daguer').checked;
+var optibox = document.getElementById('opti').checked;
+
+slidervar.noUiSlider.on('update', function( values, handle ) {
+    console.log(handle);
+    if (handle==0){
+        document.getElementById('input-number-min').value = values[0];
+    } else {
+        document.getElementById('input-number-max').value =  values[1];
+    }
+    rangeMin = document.getElementById('input-number-min').value;
+    rangeMax = document.getElementById('input-number-max').value;
+
+    //first let's clear the layer:
+    extractgroup.removeLayer(extract);
+    //and repopulate it
+    extract = new L.geoJson(null,{
+        onEachFeature: onEachFeature,
+        filter:
+            function(feature, layer) {
+                if ((photobox == true) && (daguerbox == true) && (optibox == true)){
+                    return ((feature.properties.year <= rangeMax) && (feature.properties.year >= rangeMin)) && ((feature.properties.has_photo == 1) || (feature.properties.has_daguer == 1) || (feature.properties.has_opti == 1));
+                } else if ((photobox == true) && (daguerbox == false) && (optibox == false)) {
+                    return ((feature.properties.year <= rangeMax) && (feature.properties.year >= rangeMin)) && (feature.properties.has_photo == 1);
+                } else if ((photobox == false) && (daguerbox == true) && (optibox == false)) {
+                    return ((feature.properties.year <= rangeMax) && (feature.properties.year >= rangeMin)) && (feature.properties.has_daguer == 1);
+                } else if ((photobox == false) && (daguerbox == false) && (optibox == true)) {
+                    return ((feature.properties.year <= rangeMax) && (feature.properties.year >= rangeMin)) && (feature.properties.has_opti == 1);
+                } else if ((photobox == true) && (daguerbox == false) && (optibox == true)) {
+                    return ((feature.properties.year <= rangeMax) && (feature.properties.year >= rangeMin)) && ((feature.properties.has_photo == 1) || (feature.properties.has_opti == 1));
+                } else if ((photobox == true) && (daguerbox == true) && (optibox == false)) {
+                    return ((feature.properties.year <= rangeMax) && (feature.properties.year >= rangeMin)) && ((feature.properties.has_photo == 1) || (feature.properties.has_daguer == 1));
+                } else if ((photobox == false) && (daguerbox == true) && (optibox == true)) {
+                    return ((feature.properties.year <= rangeMax) && (feature.properties.year >= rangeMin)) && ((feature.properties.has_opti == 1) || (feature.properties.has_daguer == 1));
+                }
+            },
+        pointToLayer: pointToLayerExtract
+    })
+    $.getJSON(url_extract, function(data) {
+        extract.addData(data);
+    });
+    //and back again into the cluster group
+    extract.addTo(extractgroup)
+});
+
+});
