5752643b606652e66c4ecb36bb77bf3b5363921a
solenn-tl <solenn.tual@gmail.com>
2022-10-24 17:27:13 +0200

    Update legend

--- features-endpoint.js
+++ features-endpoint.js
@@ -1,26 +1,26 @@
 //Requête SPARQL pour récupérer les données sur les monuments historiques de Paris		
-var query = "PREFIX adb: <http://data.soduco.fr/def/annuaire#>"
-+ " PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
-+ " PREFIX gsp: <http://www.opengis.net/ont/geosparql#>"
-+ " select ?uri ?index ?person ?activity ?address ?address_geocoding ?geom_wkt ?directoryName ?directoryDate"
-+ " where { ?uri a adb:Entry ;"
-+ "        rdfs:label ?person ;"
-+ "        adb:activity ?activity ;"
-+ "        adb:address ?address ;"
-+ "        adb:numEntry ?index ;"
-+ "        adb:address_geocoding ?address_geocoding ;"
-+ "        gsp:asWKT ?geom_wkt ;"
-+ "        adb:directoryName ?directoryName ;"
-+ "        adb:directoryDate ?directoryDate .}"
+var query = "PREFIX adb: <http://data.soduco.fr/def/annuaire#>"+
+"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
+"PREFIX gsp: <http://www.opengis.net/ont/geosparql#>"+
+"select ?uri ?index ?person ?activity ?address ?address_geocoding ?geom_wkt ?directoryName ?directoryDate"+
+"where { ?uri a adb:Entry ;"+
+"rdfs:label ?person ;"+
+"adb:activity ?activity ;"+
+"adb:address ?address ;"+
+"adb:numEntry ?index ;"+
+"adb:address_geocoding ?address_geocoding ;"+
+"gsp:asWKT ?geom_wkt ;"+
+"adb:directoryName ?directoryName ;"+
+"adb:directoryDate ?directoryDate .}"
 
 //On encode la requête pour l'envoyer par HTTP				
-var queryURL = "blob:http://localhost:7200/repositories/photographes-inf?query="+encodeURIComponent(query);
+var queryURL = "http://localhost:7200/repositories/photographes-inf?query="+encodeURIComponent(query);
 
 var json = $.ajax({
     url: queryURL,
-    accepts: "application/sparql-results+json",
-    contentType:"application/sparql-results+json;charset=UTF-8",
-    dataType:"json",
+    Accept: "application/sparql-results+json",
+    contentType:"application/json",
+    //dataType:"json",
     success: function (data) {
         alert(data);
     }
